ec5fd495c2aae7c86602edfeb58acdc875495a3a
AleksaLuki <dimirtiy9000@gmail.com>
2023-08-04 12:39:23 +0300

    map photo

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,23 +1,49 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { StyleSheet, Keyboard, Dimensions, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { useRouting } from './router';
+import "react-native-gesture-handler";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import LoginScreen from "./src/screens/authScreens/LoginScreen";
+import RegistrationScreen from "./src/screens/authScreens/RegistrationScreen";
+import Home from "./src/screens/mainScreens/Home";
 
-const windowDimensions = Dimensions.get('window').width - (20 * 2);
+import { useFonts } from "expo-font";
 
-export default function App() {
+const AuthStack = createStackNavigator();
 
-  const [dimensions, setDimensions] = useState({
-    window: windowDimensions,
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
   });
 
-
-  const routing = useRouting(true);
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <NavigationContainer>
-      {routing}
+      <AuthStack.Navigator initialRouteName="Login">
+        <AuthStack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{ headerShown: false }}
+        />
+        <AuthStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <AuthStack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </AuthStack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
